1cf166a781c7735f8d380d219263dc5bbbc53e4e
igor-1987 <igor.gayvoronsky@gmail.com>
2024-10-15 13:49:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+//========= lesson 5: Variables and srict mode =======//
+// "use strict"; - strict mode
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(number);
+// console.log(leftBorderWidth);
+
+// visibility of var and const, hoisting 
+// {
+//   var res_var = 50;
+// }
+// console.log(res_var);
+
+// {
+//   const res_const = 50;
+// }
+// console.log(res_const);
+
+// console.log('num: ' + num);
+// num = 6;
+// num += 14;
+// console.log('num: ' + num);
+// var num;
+
+// console.log(name)
+// var name = 'Ivan'
+
+//== как таковых констант в js несуществует! Пример:
+const obj = {
+  a: 50
+};
+
+console.log(obj);
+
+obj.a = 20;
+console.log(obj);
+
+
+
+//===================== lesson 6 ==========================//
